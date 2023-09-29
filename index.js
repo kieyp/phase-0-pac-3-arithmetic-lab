d4eb3e66dffee420d13db915419bc65661b057d0
Boniface <Cheruiyotbony@gmail.com>
2023-09-29 12:50:03 +0300

    completed

---
+++ index.js
@@ -0,0 +1,93 @@
+function add(add)
+{
+  
+  console.log(add)
+
+}
+
+function multiply(multiply)
+{
+  
+  console.log(multiply)
+
+}
+
+
+function subtract(subtract)
+{
+  
+  console.log(subtract)
+
+}
+
+
+function divide(divide)
+{
+  
+  console.log(divide)
+
+}
+
+function add(a,b)
+
+
+{
+  return a+b
+
+}
+
+function multiply(a,b)
+
+
+{
+  return a*b
+
+}
+
+
+function divide(a,b)
+
+
+{
+  return a/b
+
+}
+
+function increment(n)
+
+
+{
+  return ++n
+
+}
+
+function decrement(n)
+
+
+{
+  return --n
+
+}
+
+function subtract(a,b)
+
+
+{
+  return a-b
+
+}
+
+
+
+
+function preserveDecimal(n){
+
+  return parseFloat(n);
+}
+
+
+
+
+function makeInt(n){
+
+  return parseInt(n, 10);}
